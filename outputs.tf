b5bfbe510add91662e39d73c64325724c4c3aad1
Preet Chauhan <preetchauhan421@gmail.com>
2026-03-16 14:14:14 +0530

    fix: Module Outputs

--- outputs.tf
+++ outputs.tf
@@ -6,179 +6,179 @@ output "eventbridge_bus_name" {
 
 output "eventbridge_bus_arn" {
   description = "The EventBridge Bus ARN"
-  value       = try(aws_cloudwatch_event_bus.this[0].arn, "")
+  value       = module.eventbridge.eventbridge_bus_arn
 }
 
 # EventBridge Archive
 output "eventbridge_archive_arns" {
   description = "The EventBridge Archive ARNs"
-  value       = { for v in aws_cloudwatch_event_archive.this : v.name => v.arn }
+  value       = module.eventbridge.eventbridge_archive_arns
 }
 
 # EventBridge Permission
 output "eventbridge_permission_ids" {
   description = "The EventBridge Permission IDs"
-  value       = { for k, v in aws_cloudwatch_event_permission.this : k => v.id }
+  value       = module.eventbridge.eventbridge_permission_ids
 }
 
 # EventBridge Connection
 output "eventbridge_connection_ids" {
   description = "The EventBridge Connection IDs"
-  value       = { for k, v in aws_cloudwatch_event_connection.this : k => v.id }
+  value       = module.eventbridge.eventbridge_connection_ids
 }
 
 output "eventbridge_connection_arns" {
   description = "The EventBridge Connection Arns"
-  value       = { for k, v in aws_cloudwatch_event_connection.this : k => v.arn }
+  value       = module.eventbridge.eventbridge_connection_arns
 }
 
 # EventBridge Destination
 output "eventbridge_api_destination_arns" {
   description = "The EventBridge API Destination ARNs"
-  value       = { for k, v in aws_cloudwatch_event_api_destination.this : k => v.arn }
+  value       = module.eventbridge.eventbridge_api_destination_arns
 }
 
 # EventBridge Rule
 output "eventbridge_rule_ids" {
   description = "The EventBridge Rule IDs"
-  value       = { for k, v in aws_cloudwatch_event_rule.this : k => v.id }
+  value       = module.eventbridge.eventbridge_rule_ids
 }
 
 output "eventbridge_rule_arns" {
   description = "The EventBridge Rule ARNs"
-  value       = { for k, v in aws_cloudwatch_event_rule.this : k => v.arn }
+  value       = module.eventbridge.eventbridge_rule_arns
 }
 
 # EventBridge Schedule Groups
 output "eventbridge_schedule_group_ids" {
   description = "The EventBridge Schedule Group IDs"
-  value       = { for k, v in aws_scheduler_schedule_group.this : k => v.id }
+  value       = module.eventbridge.eventbridge_schedule_group_ids
 }
 
 output "eventbridge_schedule_group_arns" {
   description = "The EventBridge Schedule Group ARNs"
-  value       = { for k, v in aws_scheduler_schedule_group.this : k => v.arn }
+  value       = module.eventbridge.eventbridge_schedule_group_arns
 }
 
 output "eventbridge_schedule_group_states" {
   description = "The EventBridge Schedule Group states"
-  value       = { for k, v in aws_scheduler_schedule_group.this : k => v.state }
+  value       = module.eventbridge.eventbridge_schedule_group_states
 }
 
 # EventBridge Schedule
 output "eventbridge_schedule_ids" {
   description = "The EventBridge Schedule IDs created"
-  value       = { for k, v in aws_scheduler_schedule.this : k => v.id }
+  value       = module.eventbridge.eventbridge_schedule_ids
 }
 
 output "eventbridge_schedule_arns" {
   description = "The EventBridge Schedule ARNs created"
-  value       = { for k, v in aws_scheduler_schedule.this : k => v.arn }
+  value       = module.eventbridge.eventbridge_schedule_arns
 }
 
 # IAM Role
 output "eventbridge_role_arn" {
   description = "The ARN of the IAM role created for EventBridge"
-  value       = try(aws_iam_role.eventbridge[0].arn, "")
+  value       = module.eventbridge.eventbridge_role_arn
 }
 
 output "eventbridge_role_name" {
   description = "The name of the IAM role created for EventBridge"
-  value       = try(aws_iam_role.eventbridge[0].name, "")
+  value       = module.eventbridge.eventbridge_role_name
 }
 
 # EventBridge Pipes
 output "eventbridge_pipe_ids" {
   description = "The EventBridge Pipes IDs"
-  value       = { for k, v in aws_pipes_pipe.this : k => v.id }
+  value       = module.eventbridge.eventbridge_pipe_ids
 }
 
 output "eventbridge_pipe_arns" {
   description = "The EventBridge Pipes ARNs"
-  value       = { for k, v in aws_pipes_pipe.this : k => v.arn }
+  value       = module.eventbridge.eventbridge_pipe_arns
 }
 
 # IAM Role for EventBridge Pipes
 output "eventbridge_pipe_role_arns" {
   description = "The ARNs of the IAM role created for EventBridge Pipes"
-  value       = { for k, v in aws_iam_role.eventbridge_pipe : k => v.arn }
+  value       = module.eventbridge.eventbridge_pipe_role_arns
 }
 
 output "eventbridge_pipe_role_names" {
   description = "The names of the IAM role created for EventBridge Pipes"
-  value       = { for k, v in aws_iam_role.eventbridge_pipe : k => v.name }
+  value       = module.eventbridge.eventbridge_pipe_role_names
 }
 
 # Resources
 output "eventbridge_bus" {
   description = "The EventBridge Bus created and their attributes"
-  value       = aws_cloudwatch_event_bus.this
+  value       = module.eventbridge.eventbridge_bus
 }
 
 output "eventbridge_archives" {
   description = "The EventBridge Archives created and their attributes"
-  value       = aws_cloudwatch_event_archive.this
+  value       = module.eventbridge.eventbridge_archives
 }
 
 output "eventbridge_permissions" {
   description = "The EventBridge Permissions created and their attributes"
-  value       = aws_cloudwatch_event_permission.this
+  value       = module.eventbridge.eventbridge_permissions
 }
 
 output "eventbridge_connections" {
   description = "The EventBridge Connections created and their attributes"
-  value       = aws_cloudwatch_event_connection.this
+  value       = module.eventbridge.eventbridge_connections
   sensitive   = true
 }
 
 output "eventbridge_api_destinations" {
   description = "The EventBridge API Destinations created and their attributes"
-  value       = aws_cloudwatch_event_api_destination.this
+  value       = module.eventbridge.eventbridge_api_destinations
 }
 
 output "eventbridge_targets" {
   description = "The EventBridge Targets created and their attributes"
-  value       = aws_cloudwatch_event_target.this
+  value       = module.eventbridge.eventbridge_targets
 }
 
 output "eventbridge_rules" {
   description = "The EventBridge Rules created and their attributes"
-  value       = aws_cloudwatch_event_rule.this
+  value       = module.eventbridge.eventbridge_rules
 }
 
 output "eventbridge_schedule_groups" {
   description = "The EventBridge Schedule Groups created and their attributes"
-  value       = aws_scheduler_schedule_group.this
+  value       = module.eventbridge.eventbridge_schedule_groups
 }
 
 output "eventbridge_schedules" {
   description = "The EventBridge Schedules created and their attributes"
-  value       = aws_scheduler_schedule.this
+  value       = module.eventbridge.eventbridge_schedules
 }
 
 output "eventbridge_pipes" {
   description = "The EventBridge Pipes created and their attributes"
-  value       = aws_pipes_pipe.this
+  value       = module.eventbridge.eventbridge_pipes
 }
 
 # EventBridge Log Delivery Source
 output "eventbridge_log_delivery_source_arn" {
   description = "The EventBridge Bus CloudWatch Log Delivery Source ARN"
-  value       = try(aws_cloudwatch_log_delivery_source.this[0].arn, "")
+  value       = module.eventbridge.eventbridge_log_delivery_source_arn
 }
 
 output "eventbridge_log_delivery_source_name" {
   description = "The EventBridge Bus CloudWatch Log Delivery Source Name"
-  value       = try(aws_cloudwatch_log_delivery_source.this[0].name, "")
+  value       = module.eventbridge.eventbridge_log_delivery_source_name
 }
 
 # IAM Roles
 output "eventbridge_pipes_iam_roles" {
   description = "The EventBridge Pipes IAM roles created and their attributes"
-  value       = aws_iam_role.eventbridge_pipe
+  value       = module.eventbridge.eventbridge_pipes_iam_roles
 }
 
 output "eventbridge_iam_roles" {
   description = "The EventBridge IAM roles created and their attributes"
-  value       = aws_iam_role.eventbridge
+  value       = module.eventbridge.eventbridge_iam_roles
 }
