e22d64f0d0d671b19229b7543e7ba70ed96e1d36
Preet Chauhan <preetchauhan421@gmail.com>
2026-03-16 14:07:52 +0530

    feat: Made main.tf easy

--- locals.tf
+++ locals.tf
@@ -4,8 +4,8 @@ locals {
     Github_repo = "pc-eventbridge-wrapper"
   }
   owner     = "pc"
-  bus_name  = "${local.owner}-${var.bus_name}-${var.region}-${var.environment}-bus"
-  role_name = "${local.owner}-${var.role_name}-${var.region}-${var.environment}-${var.event_source_name}-role"
+  bus_name  = var.bus_name == "" ? "${local.owner}-${var.bus_name}-${var.region}-${var.environment}-bus" : var.bus_name
+  role_name = var.role_name == "" ? "${local.owner}-${var.bus_name}-${var.region}-${var.environment}-role" : var.role_name
   role_tags = merge(local.common_tags, var.role_name)
   tags      = merge(local.common_tags, var.bus_name, var.role_name)
 }
