7262cd86992a2eb24a6614b4290dc80300313302
Preet Chauhan <preetchauhan421@gmail.com>
2026-03-17 10:23:18 +0530

    fix: local tags issue

--- locals.tf
+++ locals.tf
@@ -6,6 +6,6 @@ locals {
   owner     = "pc"
   bus_name  = var.bus_name == "" ? "${local.owner}-${var.bus_name}-${var.region}-${var.environment}-bus" : var.bus_name
   role_name = var.role_name == "" ? "${local.owner}-${var.bus_name}-${var.region}-${var.environment}-role" : var.role_name
-  role_tags = merge(local.common_tags, var.role_name)
-  tags      = merge(local.common_tags, var.bus_name, var.role_name)
+  role_tags = merge(local.common_tags, {Name = local.role_name})
+  tags      = merge(local.common_tags, { bus_name = local.bus_name }, { Name = local.role_name })
 }
